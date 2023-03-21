2faa4754a69f6cd59e2da2649c1976c48ac12a17
Faez <faezlatiff0706@gmail.com>
2023-03-21 18:08:19 +0800

    added filtering according to diet

--- microservices/food_info/food_info.py
+++ microservices/food_info/food_info.py
@@ -2,10 +2,14 @@
 # import relevant dependencies
 import os
 import haversine as hs
+import json
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy 
 from flask_cors import CORS
 from datetime import datetime
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
 
 # INITIALISING APP
 app = Flask(__name__)
@@ -23,7 +27,24 @@ db = SQLAlchemy(app)
 CORS(app)
 
 # DECLARING DATABASE CLASS
-class food_db(db.Model):
+class diet_table(db.Model):
+    __tablename__ = 'dietary_table'
+
+    post_id = db.Column(db.Integer, ForeignKey('food_table.post_id', ondelete="CASCADE"), primary_key=True)
+    dietary_type = db.Column(db.VARCHAR(64), primary_key=True)
+
+    def __init__(self, post_id, dietary_type):
+        self.post_id = post_id
+        self.dietary_type = dietary_type
+    
+    def json(self):
+        diet = {
+            'post_id': self.post_id,
+            'dietary_type' : self.dietary_type
+        }
+        return diet
+
+class food_table(db.Model):
     __tablename__ = 'food_table'
 
     post_id = db.Column(db.Integer, primary_key=True)
@@ -32,14 +53,14 @@ class food_db(db.Model):
     latitude =  db.Column(db.Float(precision=6), nullable=False)
     longitude = db.Column(db.Float(precision=6), nullable=False)
     address = db.Column(db.VARCHAR(128), nullable=False)
-    description = db.Column(db.VARCHAR(300))
-    allergens = db.Column(db.VARCHAR(64))
+    description = db.Column(db.VARCHAR(248))
     is_available = db.Column(db.Integer(), nullable=False)
     end_time = db.Column(db.DateTime(), nullable=False)
     photo_name = db.Column(db.VARCHAR(64))
     photo_path = db.Column(db.VARCHAR(128))
+    diets = relationship("diet_table", backref="food_table")
 
-    def __init__(self, post_id, username, post_name, latitude, longitude, address, description, allergens, is_available, end_time, photo_name, photo_path):
+    def __init__(self, post_id, username, post_name, latitude, longitude, address, description, is_available, end_time, photo_name, photo_path):
         self.post_id = post_id
         self.username = username # username of creator
         self.post_name = post_name
@@ -47,7 +68,6 @@ class food_db(db.Model):
         self.longitude = longitude
         self.address = address
         self.description = description
-        self.allergens = allergens
         self.is_available = is_available
         self.end_time = end_time
         self.photo_name = photo_name
@@ -62,7 +82,6 @@ class food_db(db.Model):
             'longitude': self.longitude,
             'address': self.address,
             'description': self.description,
-            'allergens' : self.allergens, 
             'is_available' : self.is_available,
             'end_time' : self.end_time,
             'photo_name' : self.photo_name,
@@ -78,7 +97,7 @@ def main_page():
 
 @app.route("/all")
 def all():
-    food_list = food_db.query.all()
+    food_list = food_table.query.all()
     if len(food_list):
         return jsonify(
             {
@@ -96,9 +115,9 @@ def all():
     ), 404
 
 # RETRIEVE SPECIFIC POST
-@app.route("/search/<string:post_id>")
+@app.route("/search/<int:post_id>")
 def find_post(post_id):
-    post = food_db.query.filter_by(post_id=post_id).first()
+    post = food_table.query.filter_by(post_id=post_id).first()
 
     #if post exists, return post json
     if post:
@@ -118,24 +137,12 @@ def find_post(post_id):
     ), 404
 
 # CREATE A POST
-@app.route("/create/<int:post_id>", methods=['POST'])
-def create_post(post_id):
+@app.route("/create>", methods=['POST'])
+def create_post():
 
     #check if post is already in the db
-    if(food_db.query.filter_by(post_id=post_id).first()):
-        return jsonify(
-            {
-                "code": 400,
-                "data": {
-                    "post_id": post_id
-                },
-                "message": "Post already exists."
-            }
-        ), 400
-    
-    #else, carry on making the post
     data = request.get_json()
-    post = food_db(**data, is_available=1)
+    post = food_table(**data, is_available=1)
 
     #attempt to add post into db
     try:
@@ -147,9 +154,6 @@ def create_post(post_id):
         return jsonify(
             {
                 "code":500,
-                "data":{
-                    "post_id":post_id
-                },
                 "message": "An error occurred when creating a post. Please check if all fields meet the constraints of the database."
             }
         ), 500
@@ -263,16 +267,36 @@ def edit(post_id):
             "message": "Post not found."
         }
     ), 404
-    
+
+# TO TEST IF FOREIGN KEY FUNCTION WORKS
+@app.route("/test")
+def test():
+    post = food_table.query.filter_by(post_id=1).first()
+    if post:
+        # check diets of the post by accessing .diets, which accesses dietary_table
+        diet_list = post.diets
+        return jsonify({
+            "code":200,
+            "diets": [restriction.json()["dietary_type"] for restriction in post.diets]
+        }) 
+    return jsonify(
+        {
+            "code": 404,
+            "message" : "Post does not exist"
+        }
+    ), 404
+
 '''
 Details for wrapper function below
 
-Function: search for food posts which are within a specified user's travel appetite
+Function: search for food posts which are within a specified user's travel appetite and user's dietary requirements
 Input: user JSON object
 Output: array of food post JSON objects that fulfill the criteria
 '''
 # NEED TO FILTER ACCORDING TO ALLERGY TOO
+
 @app.route("/filter_post", methods=['GET'])
+
 # search for users that are within the distance
 def filter_post():
     # check input format and data is JSON
@@ -284,11 +308,13 @@ def filter_post():
 
             # do the actual checking
             # return list of food post objects from food_dB
-            all_food_info = food_db.query.all()
+            all_food_info = food_table.query.all()
             filtered_food = []
             if len(all_food_info):
                 # filter for posts within specified user's travel appetite
                 for food in all_food_info:
+                    
+                    post_id = food.post_id
                     food_latitude = food.latitude
                     food_longitude = food.longitude
                     user_latitude = query['latitude']
@@ -296,14 +322,25 @@ def filter_post():
                     user_travel_appetite = query['travel_appetite']
                     distance = hs.haversine((food_latitude,food_longitude),(user_latitude, user_longitude))
 
-                    if distance <= user_travel_appetite:
+                    # check dietary_list of the post by accessing .diets, which accesses dietary_table
+                    user_dietary_type = query['dietary_type']
+                    food_dietary_list = food.diets[0].query.filter_by(post_id=post_id)
+
+                    diet_list = []
+                    for diets in food_dietary_list:
+                        diet_list.append(diets.dietary_type)
+
+                    if distance <= user_travel_appetite and user_dietary_type in diet_list:
                         filtered_food.append(food)
-                # return list of food post objects where the post is within the user's travel appetite
+                
+                for food in filtered_food:
+                    
+
                 return jsonify(
                     {
-                        "code": 200,
-                        "data": {
-                            "user": [info.json() for info in filtered_food]
+                        "code":200,
+                        "data":{
+                            "filtered_food": [x.json() for x in filtered_food]
                         }
                     }
                 )
@@ -316,8 +353,13 @@ def filter_post():
                         "message": "No information to be displayed."
                     }
                 ), 404
-        except:
-            pass
+        except Exception as e:
+            return jsonify(
+                {
+                    "code": 404,
+                    "message": "Error occurred. System message: " + str(e)
+                }
+            ), 404
 
 # for guest users
 @app.route("/nearby_food", methods=['GET'])
