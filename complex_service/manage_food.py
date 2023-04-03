3a47701d171ea9b24692a5eacc2c53554d26d262
rrachea <rrachelsng@gmail.com>
2023-04-03 21:32:01 +0800

    change localhost to num

--- complex_service/manage_food.py
+++ complex_service/manage_food.py
@@ -3,10 +3,25 @@ from flask_cors import CORS
 import os, sys
 from invokes import invoke_http
 from invoke_activity import activity_log
+
+# sys.path.append('../microservices/food_announcement')
+# import sys
+# sys.path.insert(0, '../microservices/food_announcement/')
+
+# from food_announcement import amqp_setup
+import amqp_setup
+
+# from amqp_setup import amqp_setup
+import pika
+import json
+
 app = Flask(__name__)
 CORS(app)
 
-create_post_URL = 'http://localhost:1112/create_post'
+create_post_URL = 'http://127.0.0.1:1112/create_post'
+user_URL = "http://127.0.0.1:1111/profile"
+
+publish_msg_URL = "http://127.0.0.1:5100/test"
 
 '''DOCUMENTATION HERE
 User posts a new food post
@@ -29,18 +44,25 @@ output(json):
 }
 
 '''
-@app.route("/post", methods=['POST'])
+@app.route("/post", methods=['POST', 'GET'])
 def post_food():
     print('\n-----Invoking food_info microservice-----')
 
     if request.is_json:
         try:
+            data = request.get_json()
+            username = data['username']
             post_result = invoke_http(create_post_URL, method='POST', json=request.json)
             print('Post status:', post_result)
-            code = post_result["çode"]
+            code = post_result["code"]
             # CHECK OUTPUT
             # print(jsonify(post_result), post_result["code"])
             # return jsonify(post_result), post_result["code"]
+
+            print('CHECK POST RESULT')
+            # print(post_result['data']['post'])
+            print(data)
+            
         
             if code not in range(200, 300):
 
@@ -50,13 +72,43 @@ def post_food():
                     "data": {"post_result": post_result},
                     "message": "Post creation failure sent for error handling."
                 }
-            
-            return {
+
+            # when it is successful send user details + food details
+
+            url = user_URL + '/' + username
+            print(url)
+
+            user_result = invoke_http(url, method='GET', json=request.json)
+            # print(user_result)
+            username = user_result['data']['username']
+            number = user_result['data']['number']
+            email = user_result['data']['email']
+
+            user = {
+                "name": username,
+                "number":number,
+                "email":email
+            }
+
+            output = {
+
+                "food": data,
+                "user": user
+            }
+
+            # return output
+            print(f"\n this is the OUTPUT: {output} \n")
+            print(f"\nthis is the output DATATYPE: {type(output)}\n")
+            msg_result = invoke_http(publish_msg_URL, method='POST', json= output)
+
+            print(msg_result)
+
+            return jsonify({
                 "code": 201,
                 "data": {
-                    "Post_result": post_result,
+                    "Post_result": msg_result,
                 }
-            }
+            })
         
         except Exception as e:
             # Unexpected error in code
