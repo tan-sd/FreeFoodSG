e4119667af9c314f5db43751d0a8242d2227393b
Faez <faezlatiff0706@gmail.com>
2023-04-03 19:46:17 +0800

    added activity_log invocation in manage_food.py

--- complex_service/manage_food.py
+++ complex_service/manage_food.py
@@ -37,7 +37,7 @@ def post_food():
         try:
             post_result = invoke_http(create_post_URL, method='POST', json=request.json)
             print('Post status:', post_result)
-            code = post_result["çode"]
+            code = post_result["code"]
             # CHECK OUTPUT
             # print(jsonify(post_result), post_result["code"])
             # return jsonify(post_result), post_result["code"]
@@ -48,9 +48,11 @@ def post_food():
                 return {
                     "code": 500,
                     "data": {"post_result": post_result},
-                    "message": "Post creation failure sent for error handling."
+                    "message": "Error creating post."
                 }
             
+            print("Food post created successfully!")
+            activity_log("food_info")
             return {
                 "code": 201,
                 "data": {
