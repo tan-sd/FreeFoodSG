3788177627889c3962e7a87323fc4bf9ba13f63f
Faez <faezlatiff0706@gmail.com>
2023-04-04 09:58:11 +0800

    changed comments in activity_log

--- microservices/activity_log/activity_log.py
+++ microservices/activity_log/activity_log.py
@@ -55,8 +55,8 @@ class activity_table(db.Model):
 def create_log():
     '''
     this is the overarching function which creates a log and adds into database
-    input: {"ms_invoked": "microservice_name"} OR
-    input: {"ms_invoked": "microservice_name_error"}
+    input: {"log_info": "microservice_name"} OR
+    input: {"log_info": "microservice_name_error"}
     output: technically none 
     '''
     #check if not json
